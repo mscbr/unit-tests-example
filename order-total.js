26930733a3bb85582174ad1d152f065e82cecca3
mscbr <scibor.maciej@gmail.com>
2020-01-16 10:55:43 +0100

    fixes + expansion

--- order-total.js
+++ order-total.js
@@ -1,7 +1,20 @@
 function orderTotal(fetch, order) {
-  fetch(
-    'http://apilayer.net/api/rate?access_key=62fa34be411777b74645bb5006a2d3fd&country_code=DE'
-  );
+  if (order.country) {
+    return fetch(
+      `http://apilayer.net/api/rate?access_key=62fa34be411777b74645bb5006a2d3fd&country_code=${order.country}`
+    )
+      .then(res => res.json())
+      .then(data => data.standard_rate)
+      .then(
+        vat =>
+          order.items.reduce(
+            (prev, cur) => prev + cur.price * (cur.quantity || 1),
+            0
+          ) *
+          (1 + vat / 100)
+      );
+  }
+
   return Promise.resolve(
     order.items.reduce((prev, cur) => prev + cur.price * (cur.quantity || 1), 0)
   );
--- order-total.test.js
+++ order-total.test.js
@@ -3,21 +3,29 @@ const orderTotal = require('./order-total');
 const emptyFunction = () => {};
 
 it('calls apilayer.net correctly if country code specified', () => {
-  let isFakeFetchCaled = false;
+  let isFakeFetchCalled = false;
   const fakeFetch = url => {
     expect(url).toBe(
       'http://apilayer.net/api/rate?access_key=62fa34be411777b74645bb5006a2d3fd&country_code=DE'
     );
     isFakeFetchCalled = true;
+    return Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          standard_rate: 19
+        })
+    });
   };
-  orderTotal(fakeFetch, {
+
+  return orderTotal(fakeFetch, {
     country: 'DE',
     items: [
       { name: 'Dragon candy', price: 3, quantity: 1 },
       { name: 'Dragon bat', price: 213, quantity: 2 }
     ]
   }).then(res => {
-    expect(isFakeFetchCaled).toBe(true);
+    expect(res).toBe((3 * 1 + 213 * 2) * 1.19);
+    expect(isFakeFetchCalled).toBe(true);
   });
 });
 
