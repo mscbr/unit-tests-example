f980c82be8719139ab09e06681f6e7faf56cde0e
mscbr <scibor.maciej@gmail.com>
2020-01-16 10:31:58 +0100

    typo fix

--- sandbox.js
+++ sandbox.js
@@ -3,4 +3,4 @@ const result = fetch(
   'http://apilayer.net/api/rate?access_key=62fa34be411777b74645bb5006a2d3fd&country_code=DE'
 )
   .then(response => response.text())
-  .tehn(data => data.standard_rate);
+  .then(data => data.standard_rate);
